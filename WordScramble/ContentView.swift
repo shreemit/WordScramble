c02e5352bd9eaae0bc09ff3a2ba9500c2f3045a5
Shreemit <sreemith.s@humanfractal.io>
2022-03-18 11:36:12 +0530

    Loading Word List

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle.fill")
+							Text(word)
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle(rootWord)
+        .onSubmit(addNewWord)
+    }
+
+    func addNewWord() {
+        let ans = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard ans.count > 0 else { return }
+
+        withAnimation {
+            usedWords.insert(ans, at: 0)
+        }
+        newWord = ""
     }
 }
 
